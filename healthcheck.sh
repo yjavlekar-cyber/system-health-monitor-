a29720201f343e8a852b4c2b48609b15fa301d6e
yogesh <yogesh.jawlekar@gmail.com>
2026-04-23 03:26:17 +0000

    feat:add system health check script

---
+++ healthcheck.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+echo "==========SYSTEM HEALTH REPORT=========="
+echo $(date)
+
+
+echo "===CPU Usage==="
+top -bn1|grep "Cpu(s)"|awk '{print "Cpu used:" 100 - $8"%"}'
+
+
+
+echo "===Memory usage==="
+free -h | awk '/^Mem/ {print "Total: "$2 "  Used: "$3 "  Free: "$4}'
+
+
+echo "---Disk Usage---"
+df -h / | awk 'NR==2 {print "Total: "$2 "  Used: "$3 "  Free: "$4 "  Use%: "$5}'
+
+echo ""
+echo "===== End of Report ====="
+
